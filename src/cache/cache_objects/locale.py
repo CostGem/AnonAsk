69af671320fd379527df92e481388fd0f6b3cb96
CostGem <kamilcostgem@gmail.com>
2024-02-13 20:07:17 +0500

    Locale cache prefix fix

--- src/cache/cache_objects/locale.py
+++ src/cache/cache_objects/locale.py
@@ -6,5 +6,7 @@ from src.cache.cache_objects.base import BaseCache
 class LocaleCache(BaseCache):
     """Locale cache"""
 
+    prefix: str = "locale"
+
     def __init__(self, redis: Redis, user_id: int) -> None:
         super().__init__(redis=redis, item_id=user_id, ttl=300)
